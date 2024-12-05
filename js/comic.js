32432ef536600c396417f2bef2416320d5efbc3e
Jesper <153898686+jhamalainen0@users.noreply.github.com>
2024-12-05 13:49:13 +0200

    first commit

---
+++ js/comic.js
@@ -0,0 +1,37 @@
+var maxComic 
+var currentComic
+
+window.onload = function(){
+    // Hämta senaste comic 
+    getComic('latest');
+    // Sätter funktionalitet för nav knappar
+}
+
+function getComic(which) {
+    // Hämta(fetch) data från xkcd api
+    fetch('https://xkcd.vercel.app/?comic='+which)
+        .then(function(response) {
+            // Kolla om svaret är ok(200)
+            if(response.status === 200) {
+                return response.json();
+            
+            } else {
+                // Kastar ett felmeddelande om status inte är ok
+                throw new Error('Failed to load comic');
+            }
+        }) 
+        .then(function(data){
+            // Uppdatera maxComic värde
+            if (maxComic > data.num) {
+                maxComic = data.num;
+            }
+            // Skicka json data för behandling till DOM
+            console.log(data);
+            appendComic(data);
+        })
+        .catch(function(error){
+            // Logga eventuella errors
+            console.log('Error: ', error);
+        })
+}
+function appendComic(data){}
